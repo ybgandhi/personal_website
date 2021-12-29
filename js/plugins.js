5eda475d21910fd50def316dcb6af89c96fd972a
Yash Gandhi <gandhibyash@gmail.com>
2021-12-28 20:41:22 -0500

    update plugin js file

--- js/plugins.js
+++ js/plugins.js
@@ -640,17 +640,6 @@ jQuery(document).ready(function($){
 }));
 
 
-/*
-* MIXITUP - A CSS3 and JQuery Filter & Sort Plugin
-* Version: 1.5.5
-* License: Creative Commons Attribution-NoDerivs 3.0 Unported - CC BY-ND 3.0
-* http://creativecommons.org/licenses/by-nd/3.0/
-* This software may be used freely on commercial and non-commercial projects with attribution to the author/copyright holder.
-* Author: Patrick Kunka
-* Copyright 2012-2013 Patrick Kunka, Barrel LLC, All Rights Reserved
-* 
-* http://mixitup.io
-*/
 
 (function($){
     
